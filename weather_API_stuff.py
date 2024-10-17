0382e3e490a903e85766d778dfe345d0fd6b8590
gicunhads <gicunhads@gmail.com>
2024-10-17 13:03:48 +0200

    Update weather_API_stuff.py

--- weather_API_stuff.py
+++ weather_API_stuff.py
@@ -35,9 +35,10 @@ def get_top_artists():
     service = "https://dit009-spotify-assignment.vercel.app/api/v1"
     top_artists = []
     try:
-        top_tracks_week = f"{service}/playlist/37i9dQZEVXbLoATJ81JYXz/tracks?si=_O8CFC5mQ3mEEoUmGRUk0Q" # gonna get the popular tracks in the week in sweden
+        top_tracks_week = f"{service}/playlists/37i9dQZEVXbKVvfnL1Us06/tracks" # gonna get the popular tracks in the week in sweden
         response_top_tracks = requests.get(top_tracks_week)
         data_top_tracks = response_top_tracks.json()
+        print(data_top_tracks)
         for artist_id in data_top_tracks["items"]["artists"]["id"]:
             top_artists.append(artist_id)
         print(top_artists)
@@ -45,3 +46,15 @@ def get_top_artists():
         print("boo")
 
 get_top_artists()
+'''
+{'genres': ['acoustic', 'afrobeat', 'alt-rock', 'alternative', 'ambient', 'anime', 'black-metal', 'bluegrass', 'blues', 
+'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 'children', 'chill', 'classical', 'club', 'comedy', 
+'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'detroit-techno', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 
+'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 
+'grunge', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'holidays', 'honky-tonk', 'house', 'idm', 
+'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-dance', 'j-idol', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 
+'latino', 'malay', 'mandopop', 'metal', 'metal-misc', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'new-release', 
+'opera', 'pagode', 'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 
+'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-roll', 'rockabilly',
+ 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'soundtracks', 
+ 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']}'''
