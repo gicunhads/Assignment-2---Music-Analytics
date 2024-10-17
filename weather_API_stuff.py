4b02bb5fa739355705dd8777952d854d972dacf8
gicunhads <gicunhads@gmail.com>
2024-10-17 11:17:58 +0200

    Update weather_API_stuff.py

--- weather_API_stuff.py
+++ weather_API_stuff.py
@@ -7,11 +7,11 @@ response_genres = requests.get(genres)
 data_genres = response_genres.json()
 
 #after getting lat and long as inputs:
-lat =57.7089 # GOING TO BE USER INPUTS, my idea is to create a dict with some regions and the user inputs the region and then we get the lat? maybe? or just define it as sweden?
+lat =57.7089 # GOING TO BE USER INPUTS
 long =11.9746
 date_pattern = r"\(20[0-9]{2})-(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])\b" # confirm if it is YYYY-MM-DD
 
-# working:
+
 def get_average_temp():
     try:   
         weather = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&past_days=7&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
@@ -27,7 +27,7 @@ def get_average_temp():
     
     except: 
         ...
-# relating temperature to genre later on, seeing like if temp < x or y (AND REGION?)
+# relating temperature to genre (AND REGION?)
 
 
     #not working ;testing for sweden
@@ -35,7 +35,7 @@ def get_top_artists():
     service = "https://dit009-spotify-assignment.vercel.app/api/v1"
     top_artists = []
     try:
-        top_tracks_week = f"{service}/playlist/37i9dQZEVXbLoATJ81JYXz?si=_O8CFC5mQ3mEEoUmGRUk0Q/tracks" # gonna get the popular tracks in the week in sweden
+        top_tracks_week = f"{service}/playlist/37i9dQZEVXbLoATJ81JYXz/tracks?si=_O8CFC5mQ3mEEoUmGRUk0Q" # gonna get the popular tracks in the week in sweden
         response_top_tracks = requests.get(top_tracks_week)
         data_top_tracks = response_top_tracks.json()
         for artist_id in data_top_tracks["items"]["artists"]["id"]:
