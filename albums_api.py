1347692ee71548c6b7e20bdb4a20612163da9574
tyra_balg <128725591+Tyraba@users.noreply.github.com>
2024-10-23 03:58:46 +0200

    Update albums_api.py
    I am dying

--- albums_api.py
+++ albums_api.py
@@ -1,65 +1,90 @@
-import json, requests, datetime
-from datetime import datetime
-from datetime import *
+import json, requests, time
 
-
-
-
-
+service = "https://dit009-spotify-assignment.vercel.app/api/v1"
 
 def artist_id_search(name):
-    service = "https://dit009-spotify-assignment.vercel.app/api/v1"
-    id_search = f"{service}/search?q={name}&type=artist&limit=1"
-    result = requests.get(id_search)
-    result_data = result.json()
+    try:
+            artist_search = f"{service}/search?q={name}&type=artist&limit=1"
+    
+            result = requests.get(artist_search)
+            result_data = result.json()
 
 
-    for item in result_data["artists"]["items"]:
-        id = item["id"]
-        artist_name = item["name"]
-
-    right_name = input("Is " + artist_name + " the artist you are looking for? y/n").lower()
-    if right_name == "y":
-        return id
-    else:
-        id = input("Please input the right artist id: ")
-        return id
+            values_dict = {}
+            
 
 
+    
 
 
-        
+            for item in result_data["artists"]["items"]:
+               id = item["id"]
+               artist_name = item["name"]
+               genre = item["genres"]
+               popularity = item["popularity"]
+
+               values_dict[id] = [artist_name, genre, popularity]
+
+               
+            right_name = input("Is " + artist_name + " the artist you are looking for? y/n").lower()
+            if right_name == "y":
+               return values_dict
+            elif right_name =="n":
+            
+                id = input("Please input the right artist id: ")
+
+                   
+                id_search = f"{service}/artists/{id}"
+                get_id = requests.get(id_search)
+                id_file = get_id.json()
+
+                values_dict.clear()
+
+                for item in id_file:
+                   artist_name = item["name"]
+                   genre = item["genres"]
+                   popularity = item["popularity"]
+
+                   values_dict[id] = [artist_name, genre, popularity]
+
+               
+
+            else:
+                print("Error: invalid input")
+                return(None)
+               
+    except KeyError:
+        time.sleep(5)
+        artist_id_search(name)
+
+
 
 def artist_albums(id):
-    service = "https://dit009-spotify-assignment.vercel.app/api/v1"
-    album_search = f"{service}/artists/{id}/albums?limit=50&include_groups=album"
-    albums = requests.get(album_search)
-    album_file = albums.json()
-    d1 = datetime.today().date()
-    dicto = {}
-    for item in album_file["items"]:
-        name = item["name"]
-        release_date = item["release_date"]
-        dicto[name] = release_date
-        release_date1 = datetime.strptime(release_date,'%Y-%m-%d').date()
-        if d1 > release_date:
-            d1 = release_date
-
-    year = d1.year()
+    try:
+        album_search = f"{service}/artists/{id}/albums?limit=50&include_groups=album"
+        albums = requests.get(album_search)
+        album_file = albums.json()
+        total_albums = album_file["total"]
+        print(total_albums)
+        return total_albums
+    except KeyError:
+        time.sleep(10)
+        artist_albums(id)
+     
 
 
-    if len(dicto) == 50:
-        album_search2 = f"{service}/artists/{id}/albums?limit=50&include_groups=album&year:1800-{year}"#does not work
-        album_get = requests.get(album_search2)
-        album_file2 = album_get.json
-        for item in album_file2["items"]:
-            name = item["name"]
-            release_date = item["release_date"]
-            release_date1 = datetime.strptime(release_date,'%Y-%m-%d').date()
-            if d1 > release_date:
-               d1 = release_date
-            dicto[name] = release_date
-        
+def main():
+    artist_name1 = input("Input the first artist name: ")
+    artist_id1 = artist_id_search(artist_name1)
+    album_amount1 = artist_albums(artist_id1)
+    
 
 
-    print(dicto)
+    artist_name2 = input("Input the second artist name: ")
+    artist_id2 = artist_id_search(artist_name2)
+    album_amount2 = artist_albums(artist_id2)
+    
+
+
+if __name__ == "__main__":
+    main()
