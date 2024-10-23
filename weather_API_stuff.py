96d85ba8d1de9e48dd2fcf8e76fa12fc023bda01
gicunhads <gicunhads@gmail.com>
2024-10-23 21:31:50 +0200

    modified:   weather_API_stuff.py

--- weather_API_stuff.py
+++ weather_API_stuff.py
@@ -182,8 +182,8 @@ def main():
         if top_genres != None: 
             print(f"average temperature in this week is {average_temperature}°C")
             if top_genres != []:
-                country_genre[country] = top_genres
                 top_genres = [genre for sublist in top_genres for genre in sublist]
+                country_genre[country].extend(top_genres)
                 country_temp[country] = average_temperature
                 with open(f'./resources/country_genre.json', 'w') as file:
                     json.dump(country_genre, file)
