d953751f226c0c8c910ddea79e93cde6604b94cb
gicunhads <gicunhads@gmail.com>
2024-10-23 19:14:00 +0200

    Update matplotlib_avtemps_genres.py

--- matplotlib_avtemps_genres.py
+++ matplotlib_avtemps_genres.py
@@ -1,16 +1,30 @@
 import matplotlib.pyplot as plt
+import json
 
-countries = ["Canada", "Italy", "France", "United States", "India", "Brazil", "Sweden"]
-temperatures = [10.94, 28.08, 24.45, 28.50, 25.27, 27.32, 9.44]
-genres = [2, 3, 3, 1, 4, 5, 4]
+with open(f"./resources/country_genre.json", "r") as file:
+    country_genre = json.load(file)
+
+with open(f'./resources/country_temp.json', 'r') as file:
+    country_temp = json.load(file)
+
+countries = []
+temperatures = []
+number_genres = []
+
+for country, genre in country_genre.items():
+    countries.append(country)
+    number_genres.append(len(genre))
+
+for country, temp in country_temp.items():
+    temperatures.append(temp)
 
 
 plt.figure(figsize=(10, 6))
-plt.scatter(temperatures, genres, color="m")
+plt.scatter(temp, number_genres, color="m")
 
 
 for i, country in enumerate(countries):
-    plt.text(temperatures[i], genres[i] + 0.1, country, fontsize=9)
+    plt.text(temperatures[i], number_genres[i] + 0.1, country, fontsize=9)
 
 plt.title("Average Temp. vs n. of genres per country")
 plt.xlabel("Average Temperature (°C)")
